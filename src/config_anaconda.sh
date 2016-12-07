f8dd93639da5a07d419a17ba8a0e620cb599c73d
Rui Xue <rxue@ruixue.mobi>
2016-12-07 16:34:37 +0200

    configuration of anaconda tested and added

---
+++ src/config_anaconda.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#sed -i "/^PATH=.*/d" /etc/environment
+#echo "PATH=/usr/bin/anaconda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games" >> /etc/environment
+ln -fs /usr/bin/anaconda/bin/python2.7 /usr/bin/python
